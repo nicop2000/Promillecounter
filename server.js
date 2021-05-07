19a5b1df8388244614ce4ef4a3bd75975b94cb50
nicop2000 <77485281+nicop2000@users.noreply.github.com>
2021-05-07 09:46:21 +0200

    repair

---
+++ server.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('./model/user');
+require('dotenv').config()
+
+mongoose.connect('mongodb://localhost:27017/login-app-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+
+const app = express();
+app.use('/', express.static(path.join(__dirname, 'static')));
+
+app.use(bodyParser.json());
+
+app.post('/api/change-password', async (req, res) => {
+    const {token, passwordNew: plainTextPassword} = req.body;
+    console.log(req.body);
+    console.log(plainTextPassword, typeof plainTextPassword);
+    if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+        return res.json({status: 'error', error: 'Passwort nicht gültig'})
+    }
+    //TODO: plainTextPasswordcheck with RegEx
+    try {
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        const _id = user.id;
+        const password = await bcrypt.hash(plainTextPassword, 10);
+        await User.updateOne(
+            {_id},
+            {
+                $set: {password}
+            }
+        )
+        res.json({status: 'ok', data: 'Änderung erfolgreich'});
+    } catch (error) {
+        console.log(JSON.stringify(error));
+        alert('Etwas ist schiefgelaufen. Bitte logge dich aus und wieder ein')
+        return res.json({status: 'error', error: 'Token ungültig'})
+    }
+
+    console.log('JWT decoded: ', user);
+    res.json({status: 'ok'});
+})
+
+
+app.post('/api/login', async (req, res) => {
+    const {username, password} = req.body;
+    const user = await User.findOne({username}).lean()
+
+    if (!user) {
+        return res.json({status: 'error', error: 'Benutzername oder Passwort falsch'});
+    }
+
+    if (await bcrypt.compare(password, user.password)) {
+        //the username/password-combo is valid
+        const token = jwt.sign({id: user._id, username: user.username}, process.env.JWT_SECRET)
+        return res.json({status: 'ok', data: token});
+    }
+
+    return res.json({status: 'error', error: 'Benutzername oder Passwort falsch'});
+
+
+})
+
+app.post('/api/register', async (req, res) => {
+    const {username, password: plainTextPassword} = req.body;
+
+    if (!username || typeof username !== 'string') {
+        return res.json({status: 'error', error: 'Username nicht gültig'})
+    }
+    if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+        return res.json({status: 'error', error: 'Passwort nicht gültig'})
+    }
+    //TODO: plainTextPasswordcheck with RegEx
+
+    const password = await bcrypt.hash(plainTextPassword, 10);
+
+    try {
+        const response = await User.create({
+            username,
+            password
+        })
+        console.log('User created successfully: ', response);
+    } catch (error) {
+        if (error.code === 11000) {
+            //duplicate key/username
+            return res.json({status: 'error', error: 'Username already in use'});
+        }
+        console.log(JSON.stringify(error));
+        throw error
+
+
+    }
+
+    res.json({status: 'ok'})
+})
+
+app.listen(4000, () => {
+    console.log('Server up at 4000')
+});
+
+
+
+
